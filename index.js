5ce95ecc42c4cfe238078bd9d0dc0e8498ba845a
lepfau <edouard.pfauwadel@gmail.com>
2021-11-29 17:44:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+const users = [];
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+      io.emit("new user")
+  
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+      io.emit("user disconnected")
+    });
+  });
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg );
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('user name', username => {
+    io.emit('user name', username );
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("user typing", typi => {
+    socket.broadcast.emit("user typing", typi)
+  })
+} )
+
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
