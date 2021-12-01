35a54c4c6ed6ecab50f080a0a6784c5809aefbbb
lepfau <edouard.pfauwadel@gmail.com>
2021-12-01 10:30:25 +0100

    io on tout regroupe ok

--- index.js
+++ index.js
@@ -3,34 +3,33 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-const users = [];
+let users = [];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on("new user", newuser => {
+  socket.on("new user", (newuser) => {
     socket.broadcast.emit("new user", newuser)
   })
-  console.log('a user connected');
+
   socket.on('disconnect', () => {
-  io.emit('chat message', 'some user disconnected');
+    io.emit('chat message', 'a user disconnected');
   });
-});
 
-
-io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
-});
 
-io.on("connection", (socket) => {
   socket.on("user typing", typi => {
     socket.broadcast.emit("user typing", typi)
+    console.log(users)
   })
-})
+
+});
+
+
 
 
 http.listen(port, () => {
