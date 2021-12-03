ed473e38f4dd2c489528fae46609337b62e9d5f0
lepfau <edouard.pfauwadel@gmail.com>
2021-12-03 09:47:50 +0100

    deploy test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const io = require("socket.io")(http, {
   cors: {
-    origin: "https://murmuring-springs-66018.herokuapp.com/",
+    origin: port,
     methods: ["GET", "POST"],
     
     credentials: true
